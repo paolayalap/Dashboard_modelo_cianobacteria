df1c2a042720e8e305e899b0065a0557678e7728
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-09-10 12:28:00 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-st.title('🙌 Machine Learning App')
+st.title('🧪 Dashboard cyanobacteria')
 
-st.write('This is an app builds a machine learning model')
+st.info('This is an app to demostrate and analize the data to create a system to detect cyanobacteria in water bodies with machine learning.')
+st.write('First, we're going to present our reference data. This is going to have parameters like: pH, temperature, dissolve oxygen, conductivity and turbidity. ')
 
-df = pd.read_csv('https://raw.githubusercontent.com/paolayalap/Dashboard_modelo_cianobacteria/refs/heads/master/datos_lago.csv')
-df
+with st.expander('Data IBAGUA'):
+  st.write('**In 2017 and 2019, Evelyn Rodas take some measures of clorofilla type a to know how many fitoplancton was in the lake of Amatitlán.
+  She shared to us this information to create our first models**')
+  df = pd.read_csv('https://raw.githubusercontent.com/paolayalap/Dashboard_modelo_cianobacteria/refs/heads/master/datos_lago.csv')
+  df
