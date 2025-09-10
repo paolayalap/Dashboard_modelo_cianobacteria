da312c0d04ba922e1b39c95e665f206f76cead19
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-09-10 12:16:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,9 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🙌 Machine Learning App')
 
 st.write('This is an app builds a machine learning model')
+
+df = pd.read_csv('https://raw.githubusercontent.com/paolayalap/Dashboard_modelo_cianobacteria/refs/heads/master/datos_lago.csv')
+df
