0e75da9eafc496ed78a912d7ce739e4d2e9266b1
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-09-10 12:59:41 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,6 +7,6 @@ st.info('This is an app to demostrate and analize the data to create a system to
 st.write('First, we are going to present our reference data. This is going to have parameters like: pH, temperature, dissolve oxygen, conductivity and turbidity.')
 
 with st.expander('Data IBAGUA'):
-  st.write('**In 2017 and 2019, Evelyn Rodas take some measures of clorofilla type a to know how many fitoplancton was in the lake of Amatitlán. She shared to us this information to create our first models**')
+  st.write('**In 2017 and 2019, Evelyn Rodas take some measures of chlorophyll-a to know how many fitoplancton was in the lake of Amatitlán. She shared to us this information to create our first models**')
   df = pd.read_csv('https://raw.githubusercontent.com/paolayalap/Dashboard_modelo_cianobacteria/refs/heads/master/datos_lago.csv')
   df
