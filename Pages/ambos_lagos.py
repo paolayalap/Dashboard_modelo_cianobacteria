8d188065e0621d10722a57d7ae805b87602effe6
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-22 14:52:22 -0600

    Delete Pages directory

--- Pages/ambos_lagos.py
+++
@@ -1,1 +0,0 @@
-#Se agrega el modelo con 80 datos de atitlán y 80 datos de amatilán para tener un 80 porciento de datos con lagos diferentes y aplicarlos al estanque (prueba piloto 1 y prueba piloto 2)
--- Pages/lago_atitlan.py
+++
@@ -1,1 +0,0 @@
-# Se agrega el modelo con los datos de atitlán aplicados al estanque (prueba piloto 1 y prueba piloto 2)
