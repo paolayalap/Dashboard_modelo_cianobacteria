7a23d6c9ef81fc71fbbbf40db8654f160e3ff8c0
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-23 18:39:06 -0600

    Delete pages/VolverAlMenu.py

--- pages/VolverAlMenu.py
+++
@@ -1,5 +0,0 @@
-# pages/VolverAlMenu.py
-import runpy, pathlib
-# Ajusta la ruta si es necesario:
-MAIN = pathlib.Path(__file__).resolve().parents[1] / "streamlit.py"
-runpy.run_path(str(MAIN), run_name="__main__")
