08212daddb39f6e91febeb96261276321e7a2cb7
paolayalap <99574202+paolayalap@users.noreply.github.com>
2025-10-25 09:03:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,41 @@ import matplotlib.pyplot as plt
 import streamlit as st
 
 st.title("🧪 Modelos para predecir cianobacteria")
+st.markdown("Tablero complementario para el trabajo de graduación titulado:")
+st.markdown("*Diseño de un sistema de detección de cianobacteria en cuerpos de agua por medio de aprendizaje automático.*")
+st.markdown("Realizado por: Paola Andrea Ayala Pineda.")
+
+col_img, col_txt = st.columns([1, 2], gap="medium")
+
+with col_img:
+    st.image("imagenes/estanque.jpg", use_container_width=True)
+    # st.caption("Estanque del jardín Botánico de la UVG")
+
+with col_txt:
+    texto = """
+    Actualmente, la detección de cianobacterias en cuerpos de agua en lagos como lo son 
+    el de de Amatitlán y Atitlán, se realiza por medio de un  análisis  de  laboratorio. 
+    Sin embargo,  este  proceso  puede ser tardado , costoso  y  necesita  de  personal 
+    capacitado para llevarlo a cabo. 
+    
+    Es por eso que, mediante el uso de métodos de aprendizaje automático, a continuación 
+    se muestran  modelos  que  están entrenados con distintos cuerpos de agua los cuales 
+    tienen  diferencias  en  su  calidad  de  agua  para tener un método de detección de 
+    cianobacteria más amplio, económico y eficaz. 
+
+    El cuerpo de agua que se utilizó para la validación de los modelos fue el estanque del 
+    jardín botánico de la Universidad del Valle de Guatemala. 
+    """
+    st.markdown(
+        f"""
+        <div style="text-align: justify;">
+            {texto}
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
+
+
 st.write("Presiona un botón para elegir el modelo que se desee analizar.")
 
 st.set_page_config(page_title="Selector de Modelo", page_icon="🧪")
@@ -44,7 +79,7 @@ c1, c2, c3 = st.columns(3, gap="large")
 
 with c1:
     if st.button(
-        "Modelo 1",
+        "**Modelo 1**",
         help="Predice clorofila con datos de Amatitlán.",
         use_container_width=True
     ):
@@ -52,7 +87,7 @@ with c1:
 
 with c2:
     if st.button(
-        "Modelo 2",
+        "**Modelo 2**",
         help="Predice clorofila y ficocianina con datos de Atitlán.",
         use_container_width=True
     ):
@@ -60,8 +95,8 @@ with c2:
 
 with c3:
     if st.button(
-        "Modelo 3",
-        help="Predice clorofila y ficocianina con datos de Amatitlán y Atitlán.",
+        "**Modelo 3**",
+        help="Predice clorofila con datos de Amatitlán y Atitlán.",
         use_container_width=True
     ):
         go("pages/ambos_lagos.py")
